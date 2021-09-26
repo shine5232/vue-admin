72b811d60023146261953d42fcd391f6594306c3
shine5232 <2692650429@qq.com>
2021-09-26 18:29:44 +0800

    登录/注册tab选择项

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,23 +1,63 @@
 <template>
   <div id="login">
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          v-for="item in menuTab"
+          :key="item.id"
+          :class="{ current: item.current }"
+          @click="toggleMenu(item)"
+        >
+          {{ item.text }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "login",
+  data() {
+    return {
+      menuTab: [
+        { text: "登录", current: true },
+        { text: "注册", current: false },
+      ],
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach((ele) => {
+        ele.current = !ele.current;
+      });
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
 #login {
   height: 100vh;
   background: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: 0 auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+}
+.current {
+  background-color: rgba(0, 0, 0, 0.1);
 }
 </style>
